8fd36a2e7269a3ace45c68f1bca433677b1401eb
Abdel Hady <abdelhady_mohammed@rayacorp.com>
2012-08-04 16:36:55 +0200

    adding Ushhidi with 'unicorn' theme and google maps

---
+++ application/logs/2012-07-28.log.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-07-28 13:59:28 +00:00 --- error: Missing i18n entry settings.form_auth_token.The request could not be validated. Possible CSRF attack for language en_US
+2012-07-28 13:59:28 +00:00 --- error: Missing i18n entry settings.form_auth_token.The request could not be validated. Possible CSRF attack for language en_US
